2fee5e9c08107bddf4a0112ca27da8ec31d4df36
Surya Teja <72602927+surya0702@users.noreply.github.com>
2021-10-21 18:32:43 +0530

    Added Currency class and Account class
    Added a feature for chosing a currency type for Bank staff.

---
+++ Technovert.BankApp.Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technovert.BankApp.Models
+{
+    public class Currency
+    {
+        public string CurrencyCode { get; set; }
+        public string CurrencyName { get; set; }
+        public decimal CurrencyExchangeRate { get; set; }
+    }
+}

6bdde570ead94d29e388bc41e9e3502e9b94acd7
Surya Teja <72602927+surya0702@users.noreply.github.com>
2021-10-21 18:25:23 +0530

    Added a new folder for Enumarations

---
+++ Technovert.BankApp.Models/Enums/TransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technovert.BankApp.Models.Enums
+{
+    public enum TransactionType
+    {
+        Debit,
+        Credit
+    }
+}
